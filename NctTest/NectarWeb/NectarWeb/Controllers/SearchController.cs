b535010f409d8992f3f75ec411d66db2eff03b22
Christopher L <christopher.lallau@users.noreply.github.com>
2020-02-08 23:57:25 +0100

    About

--- NctTest/NectarWeb/NectarWeb/Controllers/SearchController.cs
+++ NctTest/NectarWeb/NectarWeb/Controllers/SearchController.cs
@@ -8,7 +8,7 @@ namespace NectarWeb.Controllers
 {
     public class SearchController : Controller
     {
-       [Route("Contacter/Apiculteur")]
+        [Route("Contacter/Apiculteur")]
         public IActionResult Beekeeper()
         {
             return View();
