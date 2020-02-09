f6cb03ea5a57585fc516501b9867109cb14b7890
Francois Gittenait <gittenait.francois@gmail.com>
2020-02-09 18:13:14 +0100

    Ajout des avantages en cours

---
+++ NctTest/NectarWeb/NectarWeb/Controllers/BeekeeperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NectarWeb.Controllers
+{
+  public class BeekeeperController : Controller
+  {
+    [Route("/Apiculteur")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
