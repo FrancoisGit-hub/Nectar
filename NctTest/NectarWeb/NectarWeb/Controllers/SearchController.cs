51f9ae2153eaecb3d983d80a09aab3e8d327c58a
Francois <gittenait.francois@gmail.com>
2020-04-17 03:56:27 +0200

    add thank you message to clients

--- NctTest/NectarWeb/NectarWeb/Controllers/SearchController.cs
+++ NctTest/NectarWeb/NectarWeb/Controllers/SearchController.cs
@@ -3,15 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NectarWeb.Models;
 
 namespace NectarWeb.Controllers
 {
-    public class SearchController : Controller
+  public class SearchController : Controller
+  {
+    [Route("Contacter/Apiculteur")]
+    public IActionResult Beekeeper()
     {
-       [Route("Contacter/Apiculteur")]
-        public IActionResult Beekeeper()
-        {
-            return View();
-        }
+      return View();
     }
+
+
+    [Route("Contacter/Apiculteur-resultats/{zipCode?}")]
+    public IActionResult BeekeeperResults(string zipCode)
+    {
+      List<Beekeeper> results = new List<Beekeeper>();
+      results.Add(new Beekeeper()
+      {
+        Id = 1,
+        FirstName = "Johan", 
+        LastName = "Neveux", 
+        ZipCode = "92800", 
+        City = "Asnières", 
+        Email = "johan.neveux@gmail.com", 
+        Phone = new string[]
+        {
+          "0684666461"
+        }
+      });
+
+      results.Add(new Beekeeper()
+      {
+        Id = 2,
+        FirstName = "Christopher",
+        LastName = "Lallau",
+        ZipCode = "93800",
+        City = "Bondy",
+        Email = "chris@gmail.com",
+        Phone = new string[]
+        {
+          "0684515132"
+        }
+      });
+
+      results.Add(new Models.Beekeeper()
+      {
+        Id = 3,
+        FirstName = "Francois",
+        LastName = "Gittenait",
+        ZipCode = "75018",
+        City = "Paris",
+        Email = "gittenait.francois@gmail.com",
+        Phone = new string[]
+       {
+          "0601020304"
+       }
+      });
+
+      return View(results);
+    }
+  }
 }
