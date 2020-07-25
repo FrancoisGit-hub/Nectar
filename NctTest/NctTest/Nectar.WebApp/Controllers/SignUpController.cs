59c54ccd49ca65481b8ec2f83f31be2482fb0616
FrancoisGit-hub <53494041+FrancoisGit-hub@users.noreply.github.com>
2020-07-25 16:27:12 +0200

    Change css Site.css to SignUp.css
    Addbee img

---
+++ NctTest/NctTest/Nectar.WebApp/Controllers/SignUpController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nectar.WebApp.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Discover()
+        {
+            return View();
+        }
+    }
+}
